7202e1c3cd36a8992610fb8ca358d974e290bb9c
BRUNO AQUILES DE LIMA <bal22@inf.ufpr.br>
2023-03-27 16:20:54 -0300

    Adicionado seção sobre indentação e sistema de recuperar arquivo melhorado.

--- recupera.sh
+++ recupera.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-cp .backup/navegando . 
+if [[ "$#" -ne 1 ]]; then
+    echo "Escolha qual arquivo recuperar"
+    exit 1
+fi
+
+ARQ=$1
+
+if [[ -f .backup/"$ARQ" ]]; then
+    cp .backup/"$ARQ" . 
+    echo "Arquivo $ARQ recuperado."
+else
+    echo "Arquivo não encontrado."
+fi
+
+
+    
+
+
